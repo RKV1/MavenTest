85cb3e8932b1374cda3ab4e3a43f2188de307ae6
RKV1 <vallepuravikiran@gmail.com>
2019-07-09 15:23:09 +0530

    -

--- mavendemo/src/test/java/com/ravi/package1/Demo.java
+++ mavendemo/src/test/java/com/ravi/package1/Demo.java
@@ -1,10 +1,18 @@
 package com.ravi.package1;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class Demo {
-  @Test
-  public void f() {
-	  System.out.println("Hello Ravi==========");
-  }
+	@Test
+	public void f() throws InterruptedException {
+		System.out.println("Hello Ravi==========");
+		System.setProperty("webdriver.chrome.drvier", "chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://sppoc.speridian.com:9001/siebel/app/publicsector/enu");
+		Thread.sleep(5000);
+		driver.quit();
+	}
 }
