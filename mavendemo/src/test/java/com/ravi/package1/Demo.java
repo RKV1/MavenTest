802795d4cc2f7e43a44fbd897568bff26576c46a
RKV1 <vallepuravikiran@gmail.com>
2020-03-03 15:01:52 +0530

    -

--- mavendemo/src/test/java/com/ravi/package1/Demo.java
+++ mavendemo/src/test/java/com/ravi/package1/Demo.java
@@ -8,7 +8,7 @@ import org.testng.annotations.Test;
 public class Demo {
 	@Test
 	public void f() throws InterruptedException {
-		System.out.println("Hello Ravi==========");
+		System.out.println("Hello Ravikiran==========");
 		System.setProperty("webdriver.chrome.drvier", "chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://sppoc.speridian.com:9001/siebel/app/publicsector/enu");
